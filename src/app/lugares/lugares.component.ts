ad8d4f3ba1efea745e351898d0ed7198f4b594bd
Nicolas Tato <ntato@altimetrik.com>
2017-09-07 18:10:29 -0300

    animaciones

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { LugaresService } from "../services/lugares.service";
+import { trigger, state, style, transition, animate } from "@angular/animations";
 
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.css']
+  styleUrls: ['./lugares.component.css'],
+  animations: [
+    trigger('contenedorAnimable', [
+      state('inicial', style({
+        opacity: 0,
+        backgroundColor:'green',
+        transform:'rotate3d(0,0,0,0deg)'
+      })),
+      state('final', style({
+        opacity: 1,
+        backgroundColor:'yellow',
+        transform:'rotate3d(5,10,20,30deg)'
+      })),
+      transition('inicial => final',animate(1000)),
+      transition('final => inicial',animate(500))
+    ])
+  ]
 })
 export class LugaresComponent {
   title = 'TatoSquare!';
+  state = 'final';
   /*nico= 'Nicolas';
   listo = false;
   nombre='';*/
@@ -15,6 +33,22 @@ export class LugaresComponent {
   lat: number = -34.898418;
   lng: number = -56.159007;
   lugares = null;
+  
+  animar(){
+    this.state = (this.state === 'final') ? 'inicial' : 'final';
+  }
+
+  animacionInicia(e){
+    console.log('Iniciado!');
+    console.log(e);
+
+  }
+
+  animacionTermina(e){
+    console.log('Terminado!');
+    console.log(e);
+
+  }
   constructor(private lugaresService: LugaresService){
     
     lugaresService.getLugares()
