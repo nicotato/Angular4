f1269ac2ce861568fd26957e180a77e5ccf184ec
Nicolas Tato <ntato@altimetrik.com>
2017-09-08 18:17:32 -0300

    animation favoritos

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -10,22 +10,22 @@ import { trigger, state, style, transition, animate } from "@angular/animations"
     trigger('contenedorAnimable', [
       state('inicial', style({
         opacity: 0,
-        backgroundColor:'green',
-        transform:'rotate3d(0,0,0,0deg)'
+        // backgroundColor:'green',
+        // transform:'rotate3d(0,0,0,0deg)'
       })),
       state('final', style({
         opacity: 1,
-        backgroundColor:'yellow',
-        transform:'rotate3d(5,10,20,30deg)'
+        // backgroundColor:'yellow',
+        // transform:'rotate3d(5,10,20,30deg)'
       })),
-      transition('inicial => final',animate(1000)),
-      transition('final => inicial',animate(500))
+      transition('inicial => final',animate(2000)),
+      transition('final => inicial',animate(1000))
     ])
   ]
 })
 export class LugaresComponent {
   title = 'TatoSquare!';
-  state = 'final';
+  state = 'inicial';
   /*nico= 'Nicolas';
   listo = false;
   nombre='';*/
@@ -56,6 +56,7 @@ export class LugaresComponent {
       this.lugares = lugares;
       var me = this;
       me.lugares = Object.keys(me.lugares).map(function(key){return me.lugares[key]});
+      this.state = 'final';
     }, error => {
       console.log(error);
       alert('Tenemos dificultades, disculpe las molestias Error: '+ error.statusText)
