09067c114a473040582f61f59780462fd7b45aa3
Nicolás T <nicolastato@gmail.com>
2017-09-10 21:13:38 -0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AgmCoreModule } from '@agm/core';
-import { ResaltarDirective } from "./directives/resaltar.directive";
-import { ContarClicksDirective } from "./directives/contar-clicks.directive";
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Routes, RouterModule } from "@angular/router";
-import { DetalleComponent } from "./detalle/detalle.component";
-import { LugaresComponent } from "./lugares/lugares.component";
-import { ContactoComponent } from "./contacto/contacto.component";
-import { LugaresService } from "./services/lugares.service";
+import { Routes, RouterModule } from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LugaresService } from './services/lugares.service';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { CrearComponent } from "./crear/crear.component";
-import { HttpModule } from "@angular/http";
-import { LinkifystrPipe } from "./pipes/linkifystr.pipe";
+import { CrearComponent } from './crear/crear.component';
+import { HttpModule } from '@angular/http';
+import { LinkifystrPipe } from './pipes/linkifystr.pipe';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   {path: '', component: LugaresComponent},
   {path: 'lugares', component: LugaresComponent},
   {path: 'detalle/:id', component: DetalleComponent},
   {path: 'contacto', component: ContactoComponent},
-  {path: 'crear/:id', component: CrearComponent}
+  {path: 'crear/:id', component: CrearComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDK1IjRl9sqHOK67sLhv1B9DxXOttgn77A",
-  authDomain: "tatosquare.firebaseapp.com",
-  databaseURL: "https://tatosquare.firebaseio.com",
-  storageBucket: "",
-  messagingSenderId: "202409005462"
+  apiKey: 'AIzaSyDK1IjRl9sqHOK67sLhv1B9DxXOttgn77A',
+  authDomain: 'tatosquare.firebaseapp.com',
+  databaseURL: 'https://tatosquare.firebaseio.com',
+  storageBucket: '',
+  messagingSenderId: '202409005462'
 };
 
 @NgModule({
@@ -46,7 +48,8 @@ export const firebaseConfig = {
     LugaresComponent,
     ContactoComponent,
     CrearComponent,
-    LinkifystrPipe
+    LinkifystrPipe,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
